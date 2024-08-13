6fb7c4d5e5f8153d968ed56dda7fedb73913d1e3
Luiznay <samuelnayume@gmail.com>
2024-08-13 20:26:11 -0300

    scripit.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const convertButton = document.querySelector(".convert-button")
+
+function convertvalues(){
+    console.log("funcionou")
+}
+
+convertButton.addEventListener("clik", convertvalues)
+
